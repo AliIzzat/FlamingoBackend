2539c135fccc809779da277585186f71897af51c
Ali Izzat <aliizzat@hotmail.com>
2026-03-05 01:24:13 +0300

    Fixing MyFatoorah_3

--- routes/api/mobile/payments.js
+++ routes/api/mobile/payments.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const Order = require("../../../models/Order");
-const [invoiceId, setInvoiceId] = useState(null);
 // ----------------------------
 // ENV HELPERS
 // ----------------------------
